f544f62328fd472a121d59bea18d3743f0e35dcc
Pranav778 <morepranav863@gmail.com>
2025-05-10 12:32:24 +0530

    page1 complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 tl.from(".line h1",{
     y: 150,
     stagger: 0.2, //element will come one by one insted of all coming at one time    
@@ -15,7 +16,7 @@ tl.from("#line1-part1",{
             if(count<100) 
                 h5timer.innerHTML = count++;
             else h5timer.innerHTML = count;
-        },30)
+        },25)
     }
 })
 // add scperate beacuse of animation was satrting before other
@@ -27,17 +28,53 @@ opacity: 1
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4,
+    delay:2,
 })
 
 tl.from("#page1",{
-    delay:0.2,
+    delay:0,
     y:1600,
-    opacity:0,
+    duration:0.8,
     ease:Power4
+})
+
+tl.from("nav",{
+    opacity:0,
+    delay:0.9
 })
 
 tl.to("#loader",{
     display:"none"
 })
 
+tl.from(".page1-txt h1,#hero h2,#hero h3",{
+    y:150,
+    stagger:0.2,
+    duration:0.5,
+    // delay:0.5
+})
+
+}
+
+loadingAnimation()
+
+function cursorAnimation(){
+    
+document.addEventListener("mousemove",function(dets){
+    // console.log(dets.x)
+    // console.log(dets.y)  
+    gsap.to("#crsr",{
+        left: dets.x,
+        top: dets.y
+    })
+})
+
+// use npm for making magnet of element
+Shery.makeMagnet("#nav-part h4", {
+  //Parameters are optional.
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+});
+}
+
+cursorAnimation()
